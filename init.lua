bfacec5082af365c577bf4d216dc814af952b964
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-10-08 16:18:52 -0300

    Extracs metrics from /cluster/summary
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -1,214 +1,61 @@
+-- Copyright 2015 BMC Software, Inc.
 --
--- [boundary.com] Couchbase Lua Plugin
--- [author] Valeriu Paloş <me@vpalos.com>
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
+--    http://www.apache.org/licenses/LICENSE-2.0
 --
---
---
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
---
--- Imports.
---
-local dns   = require('dns')
-local fs    = require('fs')
-local json  = require('json')
-local http  = require('http')
-local math  = require('math')
-local os    = require('os')
-local timer = require('timer')
-local tools = require('tools')
-local url   = require('url')
+local framework = require('framework')
+local Plugin = framework.Plugin
+local WebRequestDataSource = framework.WebRequestDataSource
+local auth = framework.util.auth
+local ipack = framework.util.ipack
+local parseJson = framework.util.parseJson
+local isHttpSuccess = framework.util.isHttpSuccess
 
---
--- Initialize.
---
-local _parameters        = json.parse(fs.readFileSync('param.json')) or {}
-
-local _serverHost        = _parameters.serverHost or 'localhost'
-local _serverPort        = _parameters.serverPort or 8080
-local _serverApi         = _parameters.serverApi or '/api/v1' -- Undocumented, but more future-proof.
-
-local _pollRetryCount    = tools.fence(tonumber(_parameters.pollRetryCount) or    3,   0, 1000)
-local _pollRetryDelay    = tools.fence(tonumber(_parameters.pollRetryDelay) or  100,   0, 1000 * 60 * 60)
-local _pollInterval      = tools.fence(tonumber(_parameters.pollInterval)   or 5000, 100, 1000 * 60 * 60 * 24)
-
-local _showAllTopologies = _parameters.showAllTopologies ~= false
-local _showTopologies    = _parameters.showTopologies or {}
-local _showBolts         = _parameters.showBolts ~= false
-local _showSpouts        = _parameters.showSpouts ~= false
-
--- Select topologies.
-_showTopologies.mt = { __index = {} }
-setmetatable(_showTopologies, _showTopologies.mt)
-for _, item in ipairs(_showTopologies) do
-  _showTopologies.mt.__index[item] = true
+local params = framework.params
+local ds
+for _, i in pairs(params.items) do
+  local options = {}
+  options.host = i.host
+  options.port = i.port
+  options.protocol = 'http'
+  options.auth = auth(i.username, i.password)
+  options.path = '/api/v1/cluster/summary'
+  
+  ds = WebRequestDataSource:new(options)
 end
 
---
--- Metrics source.
---
-local _source =
-  (type(_parameters.source) == 'string' and _parameters.source:gsub('%s+', '') ~= '' and _parameters.source) or
-   os.hostname()
+local plugin = Plugin:new(params, ds)
 
---
--- Get a JSON data set from the server at the given URL (includes query string)
---
-local remaining = 0
-function retrieve(location, callback)
-  remaining = remaining + 1
-  local _pollRetryRemaining = _pollRetryCount
+function plugin:onParseValues(data, extra)
+  local result = {}
+  local metric = function (...) ipack(result, ...) end
 
-  local options = url.parse(('http://%s:%d%s%s'):format(_serverHost, _serverPort, _serverApi, location))
-  options.headers = {
-    ['Accept'] = 'application/json',
-    ['Connection'] = 'keep-alive'
-  }
-
-  function handler(response)
-    if (response.status_code ~= 200) then
-      return retry("Unexpected status code " .. response.status_code .. ", should be 200!")
-    end
-
-    local data = {}
-    response:on('data', function(chunk)
-      table.insert(data, chunk)
-    end)
-    response:on('end', function()
-      local success, json = pcall(json.parse, table.concat(data))
-
-      if success then
-        remaining = remaining - 1
-        callback(json)
-      else
-        retry("Unable to parse incoming data as a valid JSON value!")
-      end
-
-      response:destroy()
-    end)
-
-    response:once('error', retry)
+  if not isHttpSuccess(extra.status_code) then
+    -- TODO: Emit error event.
+    return
   end
 
-  function retry(result)
-    if _pollRetryRemaining > 0 then
-      _pollRetryRemaining = _pollRetryRemaining - 1
-    end
-
-    if _pollRetryRemaining == 0 then
-      return error(tostring(result.message or result))
-    end
-
-    timer.setTimeout(_pollRetryDelay, perform)
+  local success, parsed = parseJson(data)
+  if not success then
+    -- TODO: Emit error event.
+    return
   end
 
-  function perform()
-    local request = http.request(options, handler)
-    request:once('error', retry)
-    request:done()
-  end
+  metric('STORM_CLUSTER_EXECUTORS', parsed.executorsTotal)
+  metric('STORM_CLUSTER_SLOTS_TOTAL', parsed.slotsTotal)
+  metric('STORM_CLUSTER_SLOTS_USED',  parsed.slotsUsed)
+  metric('STORM_CLUSTER_TASKS_TOTAL', parsed.tasksTotal)
 
-  perform()
+  return result
 end
 
---
--- Schedule poll.
---
-function schedule()
-  timer.setTimeout(_pollInterval, poll)
-end
-
---
--- Print a metric.
---
-function metric(stamp, id, value, source)
-  print(string.format('%s %s %s %d', id, value or 0, source or _source, stamp))
-end
-
---
--- Collect topology metrics per 1 second time window.
---
-function produceTopology(stamp, id)
-  retrieve(('/topology/%s?window=1'):format(id, _pollInterval), function(topology)
-
-    -- Topology-level metrics.
-    local tsrc = _source .. '/' .. id
-    metric(stamp, 'STORM_TOPOLOGY_EMITTED',         topology.topologyStats.emitted,                   tsrc)
-    metric(stamp, 'STORM_TOPOLOGY_TRANSFERRED',     topology.topologyStats.transferred,               tsrc)
-    metric(stamp, 'STORM_TOPOLOGY_ACKED',           topology.topologyStats.acked,                     tsrc)
-    metric(stamp, 'STORM_TOPOLOGY_FAILED',          topology.topologyStats.failed,                    tsrc)
-    metric(stamp, 'STORM_TOPOLOGY_COMPLETELATENCY', tonumber(topology.topologyStats.completeLatency), tsrc)
-
-    if _showBolts then
-      for _, bolt in ipairs(topology.bolts) do
-
-        -- Bolt-level metrics.
-        local bsrc = tsrc .. "/bolt-" .. bolt.boltId
-        metric(stamp, 'STORM_BOLT_EXECUTORS', bolt.executors,                     bsrc)
-        metric(stamp, 'STORM_BOLT_TASKS', bolt.tasks,                             bsrc)
-        metric(stamp, 'STORM_BOLT_EMITTED', bolt.emitted,                         bsrc)
-        metric(stamp, 'STORM_BOLT_ACKED', bolt.acked,                             bsrc)
-        metric(stamp, 'STORM_BOLT_FAILED', bolt.failed,                           bsrc)
-        metric(stamp, 'STORM_BOLT_CAPACITY', tonumber(bolt.capacity),             bsrc)
-        metric(stamp, 'STORM_BOLT_EXECUTELATENCY', tonumber(bolt.executeLatency), bsrc)
-        metric(stamp, 'STORM_BOLT_PROCESSLATENCY', tonumber(bolt.processLatency), bsrc)
-      end
-    end
-
-    if _showSpouts then
-      for _, spout in ipairs(topology.spouts) do
-
-        -- Spout-level metrics.
-        local ssrc = tsrc .. "/spout-" .. spout.spoutId
-        metric(stamp, 'STORM_SPOUT_EXECUTORS', spout.executors,                       ssrc)
-        metric(stamp, 'STORM_SPOUT_TASKS', spout.tasks,                               ssrc)
-        metric(stamp, 'STORM_SPOUT_EMITTED', spout.emitted,                           ssrc)
-        metric(stamp, 'STORM_SPOUT_ACKED', spout.acked,                               ssrc)
-        metric(stamp, 'STORM_SPOUT_FAILED', spout.failed,                             ssrc)
-        metric(stamp, 'STORM_SPOUT_COMPLETELATENCY', tonumber(spout.completeLatency), ssrc)
-      end
-    end
-  end)
-end
-
-
---
--- Collect cluster metrics.
---
-function poll()
-  local stamp = os.time()
-
-  -- Ensure previous collections have ended.
-  if remaining > 0 then
-    return schedule()
-  end
-
-  -- Trigger collection at cluster-level.
-  retrieve('/cluster/summary', function(cluster)
-    metric(stamp, 'STORM_CLUSTER_EXECUTORS',   cluster.executorsTotal)
-    metric(stamp, 'STORM_CLUSTER_SLOTS_TOTAL', cluster.slotsTotal)
-    metric(stamp, 'STORM_CLUSTER_SLOTS_USED',  cluster.slotsUsed)
-    metric(stamp, 'STORM_CLUSTER_TASKS_TOTAL', cluster.tasksTotal)
-  end)
-
-  -- Trigger topologies.
-  retrieve('/topology/summary', function(data)
-    metric(stamp, 'STORM_CLUSTER_TOPOLOGIES',  #data.topologies)
-
-    for _, topology in ipairs(data.topologies) do
-      if _showAllTopologies or _showTopologies[topology.name] or _showTopologies[topology.id] then
-        process.nextTick(function()
-          produceTopology(stamp, topology.id)
-        end)
-      end
-    end
-  end)
-
-  -- Reschedule.
-  schedule()
-end
-
---
--- Start.
---
-poll()
+plugin:run()
