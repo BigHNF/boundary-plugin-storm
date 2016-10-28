95ee0ba4d11645790e6f0adf2ad8c2f6d86715aa
JoeBmc <jose_mathew@bmc.com>
2016-10-28 11:44:54 +0530

    Error object was changed to a string in luvit 2.0
    Error object was changed to a string in luvit 2.0

--- modules/framework.lua
+++ modules/framework.lua
@@ -1464,8 +1464,8 @@ function WebRequestDataSource:fetch(context, callback, params)
   end
 
   req:propagate('error', self, function (err)
-    err.context = self
-    err.params = params
+    --err.context = self
+    --err.params = params
     return err
   end)
   req:done()
